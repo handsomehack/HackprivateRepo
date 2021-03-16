a10414ba81b4e3dff9803a8c5dcc2e9aef4e54a7
Nopparuj <eeynv1@nottingham.ac.uk>
2021-03-16 23:45:57 +0700

    Doxygen

--- 2020_20310895/Semester2/Worksheet7/maths.h
+++ 2020_20310895/Semester2/Worksheet7/maths.h
@@ -1,7 +1,11 @@
-#ifndef MATHS_H
-#define MATHS_H
-
-int add( int a, int b );
-
+// maths.h
+#ifndef MATHSLIB_MATHS_H
+#define MATHSLIB_MATHS_H
+/** @file
+* This file contains the declarations of all exported functions.
+*/
+/** Brief description
+* This description will hopefully be picked up on by doxygen
+*/
+int add(int a, int b);
 #endif
-
