18237eab1c94d893dff79a2116acd4dad60c90b0
Nopparuj <eeynv1@nottingham.ac.uk>
2021-05-14 18:44:19 +0700

    Test Doxygen

--- Semester2/Worksheet7/maths.h
+++ Semester2/Worksheet7/maths.h
@@ -2,7 +2,7 @@
 #ifndef MATHSLIB_MATHS_H
 #define MATHSLIB_MATHS_H
 /** @file
-* This file contains the declarations of all exported functions. (Test)
+* This file contains the declarations of all exported functions. (Test1)
 */
 /** Brief description
 * This description will hopefully be picked up on by doxygen
